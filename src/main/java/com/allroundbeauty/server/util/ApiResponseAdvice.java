b00c800a6029e9f956355d877a697acfbeb9a2d6
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-05 23:38:55 +0900

    feat : 커스텀 에러가 아닌경우도 ApiResponse 처리

--- src/main/java/com/allroundbeauty/server/util/ApiResponseAdvice.java
+++ src/main/java/com/allroundbeauty/server/util/ApiResponseAdvice.java
@@ -1,8 +1,10 @@
 package com.allroundbeauty.server.util;
 
 import com.allroundbeauty.server.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ApiResponseAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
@@ -27,6 +30,13 @@ public class ApiResponseAdvice implements ResponseBodyAdvice<Object> {
             return ApiResponse.builder()
                     .statusCode(errorResponse.getStatusCode())
                     .message(errorResponse.getErrorMessage())
+                    .data(null)
+                    .build();
+        }
+        if (body instanceof ProblemDetail problemDetail) {
+            return ApiResponse.builder()
+                    .statusCode(problemDetail.getStatus())
+                    .message(problemDetail.getTitle())
                     .data(null)
                     .build();
         }
