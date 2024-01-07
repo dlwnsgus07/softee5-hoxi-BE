981a7331e24dde5ce134e493e20b23517170e737
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-07 19:52:44 +0900

    feat : Call domain marge

--- src/main/java/com/allroundbeauty/server/domain/Call.java
+++ src/main/java/com/allroundbeauty/server/domain/Call.java
@@ -1,5 +1,7 @@
 package com.allroundbeauty.server.domain;
 
+import com.allroundbeauty.server.domain.user.Customer;
+import com.allroundbeauty.server.domain.user.Driver;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -15,24 +17,31 @@ public class Call {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
     private Customer customer;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "driver_id")
     private Driver driver;
+
+
     private String source;
     private String destination;
     private LocalDateTime arrivalTime;
     private double distance;
     private String requirement;
     private int deliveryFee;
-
     @Enumerated(EnumType.STRING)
     private State state;
-
     private String deliveryImage;
     private int carrierNum;
     @OneToOne
+    @JoinColumn(name = "reservation_id")
     private Reservation reservation;
     private boolean isCargo;
+    @Column(nullable = true)
+    private double position_x;
+    @Column(nullable = true)
+    private double position_y;
 }
