e84f60c912f58d1d615bb5dd878fb3e028899bc5
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-08 16:38:03 +0900

    fix : 빈 생성자 에러 수정

--- src/main/java/com/allroundbeauty/server/vo/CallCreateVo.java
+++ src/main/java/com/allroundbeauty/server/vo/CallCreateVo.java
@@ -3,9 +3,11 @@ package com.allroundbeauty.server.vo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CallCreateVo {
     @JsonProperty(value = "user_id")
     private Long userId;
--- src/main/java/com/allroundbeauty/server/vo/ReservationVo.java
+++ src/main/java/com/allroundbeauty/server/vo/ReservationVo.java
@@ -3,15 +3,17 @@ package com.allroundbeauty.server.vo;
 import com.allroundbeauty.server.domain.Reservation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReservationVo {
     private String name;
     private String phoneNumber;
     private String hotelNumber;
 
-    public Reservation voToEntity(){
+    public Reservation voToEntity() {
         return Reservation.builder().name(this.getName()).phoneNumber(this.getPhoneNumber())
                 .hotelNumber(this.getHotelNumber()).build();
     }
